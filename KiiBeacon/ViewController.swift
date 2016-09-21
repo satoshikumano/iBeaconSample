098fc693f4769e5fd5f5f5929740f88a1be825cf
satoshi <satoshi.kumano@kii.com>
2016-09-21 16:48:28 +0900

    Initial commit.

---
+++ KiiBeacon/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  KiiBeacon
+//
+//  Created by satoshi on 2016/09/21.
+//  Copyright © 2016年 Kii. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
